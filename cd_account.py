2dccce5f75e1978dc5900e4e7047002e5a7e3643
Marshall Mayer <marshall.mayer52@gmail.com>
2023-12-23 21:15:27 -0500

    uploading assignemnt

--- cd_account.py
+++ cd_account.py
@@ -1,7 +1,7 @@
 """Import the Account class from the Account.py file."""
-# ADD YOUR CODE HERE
+from Account import Account
 
-def create_cd_account(balance, interest_rate, months):
+def create_cd_account(balance, interest, months):
     """Creates a CD account, calculates interest earned, and updates the account balance.
 
     Args:
@@ -14,20 +14,20 @@ def create_cd_account(balance, interest_rate, months):
         And returns the interest earned.
     """
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
+    CD_account = Account(balance, interest)
     #  Hint: You need to add the interest as a value, i.e, 0.
-    # ADD YOUR CODE HERE
 
     # Calculate interest earned
-    # ADD YOUR CODE HERE
+    CD_interest_earned = CD_account.balance * interest
 
     # Update the CD account balance by adding the interest earned
-    # ADD YOUR CODE HERE
-
-    # Pass the updated_balance to the set balance method using the instance of the CDAccount class.
-    # ADD YOUR CODE HERE
+    CD_account.balance += CD_interest_earned
+   
+    #Pass the updated_balance to the set balance method using the instance of the CDAccount class.
+    CD_account.set_balance(CD_account.balance + CD_interest_earned)
 
     # Pass the interest_earned to the set interest method using the instance of the CDAccount class.
-    # ADD YOUR CODE HERE
+    CD_account.set_interest(CD_interest_earned)
 
     # Return the updated balance and interest earned.
-    return  # ADD YOUR CODE HERE
+    return CD_account, CD_interest_earned
--- savings_account.py
+++ savings_account.py
@@ -1,6 +1,6 @@
 """Imports the SavingsAccount class and attributes from the Account.py file."""
 # ADD YOUR CODE HERE
-
+from Account import Account
 # Define a function for the Savings Account
 def create_savings_account(balance, interest_rate, months):
     """Creates a savings account, calculates interest earned, and updates the account balance.
@@ -18,17 +18,64 @@ def create_savings_account(balance, interest_rate, months):
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
 
+
+    account = Account(balance, interest_rate)
     # Calculate interest earned
      # ADD YOUR CODE HERE
-
+    interest_earned = balance * interest_rate
     # Update the savings account balance by adding the interest earned
     # ADD YOUR CODE HERE
+    savings_account_balance= balance + interest_earned
+    # Create an instance of the SavingsAccount class
+savings_account = Account(1000, 0.05)
 
-    # Pass the updated_balance to the set balance method using the instance of the SavingsAccount class.
+# Calculate the interest earned
+interest_earned = savings_account.balance * savings_account.interest
+
+# Update the savings account balance by adding the interest earned
+updated_balance = savings_account.balance + interest_earned
+
+# Pass the updated balance to the set_balance method
+savings_account.set_balance(updated_balance)
+
+# Print the updated savings account balance
+print(savings_account.balance)
+# Pass the updated_balance to the set balance method using the instance of the SavingsAccount class.
     # ADD YOUR CODE HERE
-
+savings_account.set_balance(updated_balance)
     # Pass the interest_earned to the set interest method using the instance of the SavingsAccount class.
-    # ADD YOUR CODE HERE
+class SavingsAccount:
+    def __init__(self, balance, interest_rate):
+        self.balance = balance
+        self.interest_rate = interest_rate
 
-    # Return the updated balance and interest earned.
-    return  # ADD YOUR CODE HERE
+    def set_interest(self, interest_earned):
+        self.interest_rate = interest_earned
+
+# Create an instance of the SavingsAccount class
+savings_account = SavingsAccount(1000, 0.05)
+
+# Pass the interest_earned to the set_interest method
+interest_earned = 0.08
+savings_account.set_interest(interest_earned)
+
+    # Return the updated balance and interest earned
+def calculate_future_value(current_loan_value, months_remaining, annual_interest_rate):
+    # Calculate the interest earned
+    interest_earned = current_loan_value * (annual_interest_rate / 12) * months_remaining
+    
+    # Calculate the updated balance
+    updated_balance = current_loan_value + interest_earned
+    
+    # Return the updated balance and interest earned
+    return updated_balance, interest_earned
+new_car_loan = {
+    "current_loan_value" : 24000,
+    "months_remaining" : 6,
+    "annual_interest_rate" : 0.085,
+}
+
+updated_balance, interest_earned = calculate_future_value(new_car_loan["current_loan_value"], new_car_loan["months_remaining"], new_car_loan["annual_interest_rate"])
+
+print("Updated Balance:", updated_balance)
+print("Interest Earned:", interest_earned)
